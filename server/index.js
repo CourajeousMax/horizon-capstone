255d80799af001e69c7aa5fc7a022407fd4e0c28
Stephany Munoz <stephany.munoz8@gmail.com>
2023-06-17 21:34:49 -0400

    added port

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+require("dotenv").config();
+const { PORT } = process.env
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(PORT, () => {
+app.listen(8000, () => {
   console.log(`Running on port ${PORT}`);
 });
