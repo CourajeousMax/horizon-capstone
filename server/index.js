395cd9af03dbe7e694bf06fa5f2fe2265fad9bf0
Stephany Munoz <stephany.munoz8@gmail.com>
2023-06-19 00:38:06 -0400

    latest pull

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require("express"); // requiring the express packge
 const app = express(); // initializing the express instance
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT;
+const knex = require("knex");
+const knexConnection = require("./config/knexConnection");
+const db = knex(knexConnection.development);
 
+//importing all routes
+const usersRoute = require("./routes/usersRoute");
 
 app.use(express.json());
 app.use(cors());
